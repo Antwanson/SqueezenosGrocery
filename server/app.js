a591020ba57d31037bedb4d6279fbb2c66a27cc8
chronoshivt <75964136+chronoshivt@users.noreply.github.com>
2024-07-22 22:26:05 -0500

    add items list to discount page

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const connection = require("./db");
+// const connectiozn = require("./db");
 
 const app = express();
 const port = process.env.PORT || 3000;
